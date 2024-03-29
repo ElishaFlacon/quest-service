baebc7562aaf441a43c29dc55a4e4ccf72aada9d
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-03-30 01:01:49 +0500

    add base methods for database

--- database/index.database.go
+++ database/index.database.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -21,4 +22,36 @@ func Init(database_url string) {
 	}
 
 	Database = &TDatabase{db}
+}
+
+func BaseQuery[T comparable](sqlString string, args ...any) ([]*T, error) {
+	rows, err := Database.Query(context.Background(), sqlString, args...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := pgx.CollectRows(rows, pgx.RowToAddrOfStructByName[T])
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, err
+}
+
+// using for inserts
+func CopyFromQuery(tableName string, columnNames []string, rows [][]any) (int64, error) {
+	count, err := Database.CopyFrom(
+		context.Background(),
+		pgx.Identifier{tableName},
+		columnNames,
+		pgx.CopyFromRows(rows),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return count, err
 }
