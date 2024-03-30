0edf8d59dbb07704f44cb8d8506917bd3582a269
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-03-30 23:24:59 +0500

    remove useless fields from models

--- models/indicator.model.go
+++ models/indicator.model.go
@@ -4,7 +4,6 @@ type Indicator struct {
 	IdIndicator int    `json:"idIndicator"`
 	Value       string `json:"value"`
 	Description string `json:"description"`
-	Type        string `json:"type"`
 	Role        string `json:"role"`
 	Visible     bool   `json:"visible"`
 }
--- models/quest.model.go
+++ models/quest.model.go
@@ -2,8 +2,7 @@ package models
 
 type Quest struct {
 	IdQuest     int    `json:"idQuest"`
-	Available   bool   `json:"available"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	Link        string `json:"link"`
+	Available   bool   `json:"available"`
 }
