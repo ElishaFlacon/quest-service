ab33c9869cf9f242f6d6f60617804c5dff62604f
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-04-20 20:25:50 +0500

    mini update for team service

--- service/team.service.go
+++ service/team.service.go
@@ -3,41 +3,55 @@ package service
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/ElishaFlacon/quest-service/models"
 	"io"
-	"log"
 	"net/http"
+
+	"github.com/ElishaFlacon/quest-service/models"
+	"github.com/ElishaFlacon/quest-service/utils"
 )
 
 type TTeam struct{}
 
 var Team *TTeam
 
-func (*TTeam) GetAllMembersOfTeams(idTeams []*int) ([]*models.TeamMembers, error) {
-	var teams models.TeamsResponse
+var TeamServiceUrl = utils.GetTeamServiceUrl()
 
-	apiUrl := "/api/v1/teamservice/get-all-members"
+func (*TTeam) GetAllMembersOfTeams(
+	idTeams []*int,
+) ([]*models.TeamMembers, error) {
+	client := &http.Client{}
+	teams := models.TeamsResponse{}
 
 	jsonIdTeams, _ := json.Marshal(idTeams)
 	teamsData := []byte(`{"idTeams":` + string(jsonIdTeams))
 
-	request, err := http.NewRequest("GET", apiUrl, bytes.NewBuffer(teamsData))
-	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+	request, err := http.NewRequest(
+		"GET",
+		TeamServiceUrl,
+		bytes.NewBuffer(teamsData),
+	)
+	if err != nil {
+		return nil, err
+	}
 
-	client := &http.Client{}
+	request.Header.Set(
+		"Content-Type",
+		"application/json; charset=utf-8",
+	)
 
 	response, err := client.Do(request)
 	if err != nil {
-		log.Panic(err)
 		return nil, err
 	}
 	defer response.Body.Close()
 
 	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
 
 	err = json.Unmarshal(responseBody, &teams)
 	if err != nil {
-		log.Panic(err)
 		return nil, err
 	}
 
