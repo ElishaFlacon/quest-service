67f1344972b8269aa3fcce22d72d363cb7647a12
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-04-21 02:01:32 +0500

    add more models for quest

--- models/quest.model.go
+++ models/quest.model.go
@@ -28,3 +28,24 @@ type QuestWithIndicators struct {
 	Status     string      `json:"status"`
 	Indicators []Indicator `json:"indicators"`
 }
+
+type QuestWithUsers struct {
+	IdQuest int      `json:"id"`
+	Name    string   `json:"name"`
+	StartAt int      `json:"startAt"`
+	EndAt   int      `json:"endAt"`
+	Percent float32  `json:"percent"`
+	Status  string   `json:"status"`
+	Users   []string `json:"users"`
+}
+
+type QuestWithUsersAndIndicators struct {
+	IdQuest    int         `json:"id"`
+	Name       string      `json:"name"`
+	StartAt    int         `json:"startAt"`
+	EndAt      int         `json:"endAt"`
+	Percent    float32     `json:"percent"`
+	Status     string      `json:"status"`
+	Indicators []Indicator `json:"indicators"`
+	Users      []string    `json:"users"`
+}
