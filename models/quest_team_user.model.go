c653ab2ce72a2c030d47c9e930e4cf3dcd94d544
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-04-21 03:22:42 +0500

    add new model

--- models/quest_team_user.model.go
+++ models/quest_team_user.model.go
@@ -4,3 +4,8 @@ type QuestTeamUser struct {
 	IdQuestTeam int    `json:"idQuestTeam"`
 	IdUser      string `json:"idUser"`
 }
+
+type QuestTeamUsers struct {
+	IdQuestTeam int      `json:"idQuestTeam"`
+	Users       []string `json:"idUsers"`
+}
