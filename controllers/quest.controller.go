89f1fb20c2457dc539f36331ba1849218ccd69d6
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-04-21 01:38:19 +0500

    add full quest controller

--- controllers/quest.controller.go
+++ controllers/quest.controller.go
@@ -2,39 +2,140 @@ package controllers
 
 import (
 	"github.com/ElishaFlacon/quest-service/service"
+	"github.com/ElishaFlacon/quest-service/utils"
 	"github.com/gin-gonic/gin"
 )
-
-// Таблица
-// Деталка сам опрос + участники
-// Выдача вопросов (частично сделано в индикаторах)
-// Создание
-// удаление\скрытие (в самом конце)
 
 type TQuest struct{}
 
 var Quest *TQuest
 
-func (*TQuest) GetAll(context *gin.Context) {
-	data, err := service.Quest.GetAll()
-
-	if err != nil {
-		context.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-
-	context.JSON(200, gin.H{"data": data})
+// Quest Get	godoc
+// @Summary	Пример get quest by id
+// @Tags	quest
+// @Accept	json
+// @Produce	json
+// @Success	200	{object}	models.QuestResponse
+// @Failure	400	{string} 	string
+// @Failure	500	{string} 	string
+// @Router	/quest/:id [get]
+func (*TQuest) Get(context *gin.Context) {
+	id := utils.CultivateNumberParam(context, "id")
+	data, errData := service.Quest.Get(id)
+	utils.CultivateServiceData(context, data, errData)
 }
 
-// Quest Get	godoc
-// @Summary	Пример get quest
+// Quest GetWithIndicators	godoc
+// @Summary	Пример get quest by id with indicators
+// @Tags	quest
+// @Accept	json
+// @Produce	json
+// @Success	200	{object}	models.QuestResponse
+// @Failure	400	{string} 	string
+// @Failure	500	{string} 	string
+// @Router	/quest/with-indicators/:id [get]
+func (*TQuest) GetWithIndicators(context *gin.Context) {
+	id := utils.CultivateNumberParam(context, "id")
+	data, errData := service.Quest.GetWithIndicators(id)
+	utils.CultivateServiceData(context, data, errData)
+}
+
+// Quest GetWithUsers	godoc
+// @Summary	Пример get quest by id with users
+// @Tags	quest
+// @Accept	json
+// @Produce	json
+// @Success	200	{object}	models.QuestResponse
+// @Failure	400	{string} 	string
+// @Failure	500	{string} 	string
+// @Router	/quest/with-users/:id [get]
+func (*TQuest) GetWithUsers(context *gin.Context) {
+	id := utils.CultivateNumberParam(context, "id")
+	data, errData := service.Quest.GetWithUsers(id)
+	utils.CultivateServiceData(context, data, errData)
+}
+
+// Quest GetWithUsersAndIndicators	godoc
+// @Summary	Пример get quest by id with users and indicators
+// @Tags	quest
+// @Accept	json
+// @Produce	json
+// @Success	200	{object}	models.QuestResponse
+// @Failure	400	{string} 	string
+// @Failure	500	{string} 	string
+// @Router	/quest/with-users-and-indicators/:id [get]
+func (*TQuest) GetWithUsersAndIndicators(context *gin.Context) {
+	id := utils.CultivateNumberParam(context, "id")
+	data, errData := service.Quest.GetWithUsersAndIndicators(id)
+	utils.CultivateServiceData(context, data, errData)
+}
+
+// Quest GetAll	godoc
+// @Summary	Пример get all quest
+// @Tags	quest
+// @Accept	json
+// @Produce	json
+// @Success	200	{array}	models.QuestResponse
+// @Failure	400	{string} 	string
+// @Failure	500	{string} 	string
+// @Router	/quest/all [get]
+func (*TQuest) GetAll(context *gin.Context) {
+	data, errData := service.Quest.GetAll()
+	utils.CultivateServiceData(context, data, errData)
+}
+
+// Quest Create	godoc
+// @Summary	Пример create quest
 // @Tags	quest
 // @Accept	json
 // @Produce	json
 // @Success	200	{object}	models.Quest
 // @Failure	400	{string} 	string
 // @Failure	500	{string} 	string
-// @Router	/quest/:id [get]
-func (*TQuest) Get(context *gin.Context) {
-	// TODO
+// @Router	/quest/create [post]
+func (*TQuest) Create(context *gin.Context) {
+	body := struct {
+		Name        string                `json:"name"`
+		Description string                `json:"description"`
+		Teams       []struct{ Id string } `json:"teams"`
+	}{}
+	utils.CultivateBody(context, body)
+
+	teams := utils.CultivateId(body.Teams)
+	data, errData := service.Quest.Create(
+		body.Name,
+		body.Description,
+		teams,
+	)
+	utils.CultivateServiceData(context, data, errData)
+}
+
+// Quest Hide	godoc
+// @Summary	Пример hide quest by id
+// @Tags	quest
+// @Accept	json
+// @Produce	json
+// @Success	200	{object}	models.Quest
+// @Failure	400	{string} 	string
+// @Failure	500	{string} 	string
+// @Router	/quest/hide/:id [put]
+func (*TQuest) Hide(context *gin.Context) {
+	id := utils.CultivateNumberParam(context, "id")
+	data, errData := service.Quest.Hide(id)
+	utils.CultivateServiceData(context, data, errData)
+}
+
+// Quest Delete	godoc
+// @Summary	Пример delete quest by id
+// @Tags	quest
+// @Accept	json
+// @Produce	json
+// @Success	200	{object}	models.Quest
+// @Failure	400	{string} 	string
+// @Failure	500	{string} 	string
+// @Router	/quest/delete/:id [delete]
+func (*TQuest) Delete(context *gin.Context) {
+	id := utils.CultivateNumberParam(context, "id")
+	data, errData := service.Quest.Delete(id)
+	utils.CultivateServiceData(context, data, errData)
 }
