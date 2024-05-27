824f7e6507ac8ebaec26a982e72ca7897517c6dd
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-05-27 20:27:18 +0500

    update team model

--- models/team.model.go
+++ models/team.model.go
@@ -1,15 +1,16 @@
 package models
 
+type Member struct {
+	IdUser string `json:"id"`
+	Email  string `json:"email"`
+}
+
 type Team struct {
-	IdTeam string `json:"idTeam"`
-	Users  []User `json:"users"`
+	IdTeam string    `json:"id"`
+	Users  []*Member `json:"members"`
 }
 
-type IdeaServiceTeam struct {
-	IdTeam  string
-	IdUsers []string
-}
-
-type IdeaServiceTeams struct {
-	Teams []*IdeaServiceTeam `json:"teams"`
+type TeamWithStringUsers struct {
+	IdTeam string   `json:"id"`
+	Users  []string `json:"users"`
 }
