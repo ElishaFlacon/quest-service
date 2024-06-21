7de5be20b09c5e031ee4051493734acd515aea82
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-06-21 16:06:08 +0500

    cors off

--- config/usings.config.go
+++ config/usings.config.go
@@ -1,10 +1,9 @@
 package config
 
 import (
-	"github.com/ElishaFlacon/quest-service/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func InitUsings(app *gin.Engine) {
-	app.Use(middlewares.Cors())
+	// app.Use(middlewares.Cors())
 }
